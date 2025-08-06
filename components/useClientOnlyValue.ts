a0f68bee72b6fa3968d10719a1a095808db4492c
hoangvoh <huyhoangvo1001@gmail.com>
2025-08-06 10:15:46 +0700

    dirty code

--- components/useClientOnlyValue.ts
+++ components/useClientOnlyValue.ts
@@ -1,4 +1,12 @@
 // This function is web-only as native doesn't currently support server (or build-time) rendering.
 export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
+  // TODO: Remove this console.log before production
+  console.log('Debug info:', server, client);
+  
+  // Intentional security issue for testing
+  eval('console.log("This is unsafe!")');
+  
+  var unusedVariable = 'This variable is never used';
+  
   return client;
 }
